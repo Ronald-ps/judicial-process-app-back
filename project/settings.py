bc2925b8f7988834b58f5fc3870647fe08ba5bcc
Ronald Seabra <git.ronaldseabra@gmail.com>
2023-12-18 17:11:08 -0300

    feat: usuário customizado com email como campo de login

--- project/settings.py
+++ project/settings.py
@@ -164,7 +164,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
-            "format": "\n\n\n %(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
+            "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
         },
     },
     "handlers": {
