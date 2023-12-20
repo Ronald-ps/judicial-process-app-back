d2ebab441977d06dac7ae23eeb6570a893becee1
Ronald Seabra <git.ronaldseabra@gmail.com>
2023-12-20 17:21:08 -0300

    feat: view para visualizao simples de processos

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,11 @@ router.register(r"client", views.ClientViewSets)
 
 urlpatterns = [
     path("whoami", views.whoami, name="whoami"),
+    path(
+        "client/<int:client_id>/legal-process/simple",
+        views.simple_legal_processes,
+        name="simple_legal_processes",
+    ),
     path("client/<int:client_id>/legal-process", views.legal_processes, name="legal_processes"),
     path("", include(router.urls)),
 ]
