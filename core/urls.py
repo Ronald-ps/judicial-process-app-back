d789897ee03328a7f030a7e137bc0971ab5493ee
Ronald Seabra <git.ronaldseabra@gmail.com>
2023-12-27 01:14:32 -0300

    fix:add urls de  media url

--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path("client/<int:client_id>/legal-process", views.legal_processes, name="legal_processes"),
     path("client/<int:client_id>/profile-image/", views.profile_image, name="profile_image"),
     path("", include(router.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
